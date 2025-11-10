6afff4cbf8b45a79e4a431627c31f068b0113062
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-11-10 20:04:22 +0100

    type: text,       required: false

--- src/index.ts
+++ src/index.ts
@@ -16,17 +16,6 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap({ strapi }) {
-    try {
-      const knex = strapi.db.connection;
-
-      await knex("components_global_section_rich_texts")
-        .whereRaw(`content IS NULL OR content = '' OR content NOT LIKE '[%'`)
-        .update({ content: "[]" });
-
-      strapi.log.info("✅ Fixed invalid content in section-rich-text");
-    } catch (err) {
-      strapi.log.error("❌ Failed to fix content field:", err);
-    }
-  },
+  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
+
